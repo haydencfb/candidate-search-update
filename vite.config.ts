36d48a95789bf5ff5469a5468f3a48224fe9f2d6
haydencfb <haydenfitzbrintle@gmail.com>
2024-10-01 18:19:27 -0700

    port change

--- vite.config.ts
+++ vite.config.ts
@@ -5,4 +5,8 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   envDir: './environment',
   plugins: [react()],
+  server: { 
+    port: 8080,
+    strictPort: true,
+  }
 });
